a965d3043ce130df9a5525c1ccd1140d9a2ddbd2
Matthew Muniz <48893316+munizio@users.noreply.github.com>
2025-04-04 12:37:00 -0500

    Add files via upload

---
+++ lua/dunehazard/init.lua
@@ -0,0 +1,26 @@
+require("dunehazard.remap")
+require("dunehazard.lazy_init")
+require("dunehazard.set")
+
+local augroup = vim.api.nvim_create_augroup
+local dunehazardGroup = augroup('dunehazard', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd('LspAttach', {
+    group = dunehazardGroup,
+    callback = function(e)
+        local opts = { buffer = e.buf }
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    end
+})
+
---
+++ lua/dunehazard/lazy/telescope.lua
@@ -0,0 +1,42 @@
+return {
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    {
+        "nvim-telescope/telescope.nvim",
+
+        dependencies = {
+            "nvim-lua/plenary.nvim"
+        },
+
+        config = function()
+            local telescope = require('telescope')
+
+            telescope.setup({
+                extensions = {
+                    fzf = {
+                        fuzzy = true,                    -- false will only do exact matching
+                        override_generic_sorter = true,  -- override the generic sorter
+                        override_file_sorter = true,     -- override the file sorter
+                        case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                        -- the default case_mode is "smart_case"
+                    }
+                }})
+            telescope.load_extension('fzf')
+
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+            vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+            vim.keymap.set('n', '<leader>pws', function()
+                local word = vim.fn.expand("<cword>")
+                builtin.grep_string({ search = word })
+            end)
+            vim.keymap.set('n', '<leader>pWs', function()
+                local word = vim.fn.expand("<cWORD>")
+                builtin.grep_string({ search = word })
+            end)
+            vim.keymap.set('n', '<leader>ps', function()
+                builtin.grep_string({ search = vim.fn.input("Grep > ") })
+            end)
+            vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+        end
+    }
+}
